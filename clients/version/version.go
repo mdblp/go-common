67cda19c639d422f8921c2865ffb8d5ee040c31c
ydris <ydris@diabeloop.fr>
2019-10-15 20:54:29 +0200

    review the version package architecture
    based on peer review comments

--- clients/version/version.go
+++ clients/version/version.go
@@ -4,40 +4,71 @@ import (
 	"fmt"
 )
 
+type version struct {
+	initialized bool
+	base        string
+	shortCommit string
+	fullCommit  string
+}
+
+func (v version) getBase() string {
+	if v.base != "" {
+		return v.base
+	}
+	return "N/A"
+}
+
+func (v version) getShortCommit() string {
+	if v.shortCommit != "" {
+		return v.shortCommit
+	}
+	return "N/A"
+}
+
+func (v version) getFullCommit() string {
+	if v.fullCommit != "" {
+		return v.fullCommit
+	}
+	return v.getShortCommit()
+}
+
+func (v version) GetShort() string {
+	return fmt.Sprintf("%s+%s", v.getBase(), v.getShortCommit())
+}
+
+func (v version) String() string {
+	return fmt.Sprintf("%s+%s", v.getBase(), v.getFullCommit())
+}
+
+/**
+	Initialization wrapper
+**/
+
+// Singleton version instance
+var instance version
+
+// Variables to be injected at build time
 var (
-	VersionBase        string
-	VersionShortCommit string
-	VersionFullCommit  string
+	releaseNumber string //Release number. i.e. 1.2.3
+	shortCommit   string
+	fullCommit    string //Full commit id. i.e. e0c73b95646559e9a3696d41711e918398d557fb
 )
 
-func Base() string {
-	if VersionBase != "" {
-		return VersionBase
-	} else {
-		return "N/A"
+func init() {
+	if !instance.initialized {
+		instance = version{base: releaseNumber, fullCommit: fullCommit, shortCommit: shortCommit, initialized: true}
 	}
 }
 
-func ShortCommit() string {
-	if VersionShortCommit != "" {
-		return VersionShortCommit
-	} else {
-		return "N/A"
-	}
-}
-
-func FullCommit() string {
-	if VersionFullCommit != "" {
-		return VersionFullCommit
-	} else {
-		return ShortCommit()
-	}
-}
-
-func Short() string {
-	return fmt.Sprintf("%s+%s", Base(), ShortCommit())
+// Initialization function, only used for tests
+func forceInit() {
+	instance = version{base: releaseNumber, fullCommit: fullCommit, shortCommit: shortCommit, initialized: true}
 }
 
 func Long() string {
-	return fmt.Sprintf("%s+%s", Base(), FullCommit())
+	return instance.String()
+}
+
+func Short() string {
+	return instance.GetShort()
 }
--- clients/version/version_test.go
+++ clients/version/version_test.go
@@ -1,18 +1,17 @@
-package version_test
+package version
 
 import (
 	"testing"
-
-	"github.com/tidepool-org/go-common/clients/version"
 )
 
 func TestVersion(t *testing.T) {
-	version.VersionBase = "1.2.3"
-	version.VersionShortCommit = "e0c73b9"
-	version.VersionFullCommit = "e0c73b95646559e9a3696d41711e918398d557fb"
+	releaseNumber = "1.2.3"
+	shortCommit = "e0c73b9"
+	fullCommit = "e0c73b95646559e9a3696d41711e918398d557fb"
 
-	shortV := version.Short()
-	longV := version.Long()
+	forceInit()
+	shortV := Short()
+	longV := Long()
 
 	if shortV != "1.2.3+e0c73b9" {
 		t.Errorf("Expected short version %s but got %s", "1.2.3+e0c73b9", shortV)
