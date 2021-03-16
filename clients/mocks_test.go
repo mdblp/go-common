c10d916eb2029ae386a4a8231744e7eb8cc1e680
CedricPerotto <cedric.perotto@diabeloop.fr>
2021-03-16 19:11:21 +0100

    Upgrading seagull mock

--- clients/mocks_test.go
+++ clients/mocks_test.go
@@ -104,11 +104,25 @@ func TestSeagullMock_GetCollection(t *testing.T) {
 
 	sc := NewSeagullMock()
 	var col struct{ Something string }
+	sc.SetMockNextCollectionCall("123.456stuff", `{"Something":"anit no thing"}`, nil)
 
-	sc.GetCollection("123.456", "stuff", TOKEN_MOCK, &col)
-
+	err := sc.GetCollection("123.456", "stuff", TOKEN_MOCK, &col)
+	if err != nil {
+		t.Error("Should not return an error")
+	}
 	if col.Something != "anit no thing" {
 		t.Error("Should have given mocked collection")
+	}
+
+	errMsg := "Testing my error"
+	sc.SetMockNextCollectionCall("123.456stuff", "", errors.New(errMsg))
+
+	err = sc.GetCollection("123.456", "stuff", TOKEN_MOCK, &col)
+	if err == nil {
+		t.Error("Should return an error")
+	}
+	if err.Error() != errMsg {
+		t.Errorf("Unexepected error message expected:%v/receieved:%v", errMsg, err.Error())
 	}
 }
 
