911bac6332c90559eb7efdd86b3075d8b099cbfb
ydris <ydris@diabeloop.fr>
2022-09-28 10:51:53 +0200

    revert

--- errors/errors.go
+++ errors/errors.go
@@ -3,7 +3,6 @@ package errors
 import (
 	"errors"
 	"fmt"
-	"runtime"
 )
 
 type WrappedError struct {
@@ -29,13 +28,4 @@ func Wrap(e error, m string) error {
 
 func Wrapf(e error, m string, args ...interface{}) error {
 	return Wrap(e, fmt.Sprintf(m, args...))
-}
-
-// Create a new error which contains the stack trace
-func NewDetailedError(msg string) error {
-	pc := make([]uintptr, 15)
-	n := runtime.Callers(2, pc)
-	frames := runtime.CallersFrames(pc[:n])
-	frame, _ := frames.Next()
-	return fmt.Errorf("[%s:%d %s] : %s", frame.File, frame.Line, frame.Function, msg)
 }
