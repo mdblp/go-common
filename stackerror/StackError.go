7402534d7be0855467d36c6790e8a973e299f6fb
ydris rebibane <ydris@diabeloop.fr>
2023-02-22 18:11:20 +0100

    New proposal

---
+++ stackerror/StackError.go
@@ -0,0 +1,45 @@
+// Package StackError: custom stackerror which carry the stack trace in the error message
+package stackerror
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// StackError defines an error with details about the source (function, line number...) and other details
+type StackError struct {
+	message        string                 // error message
+	details        map[string]interface{} // optional details
+	sourceFilename string                 // name of the file from where the error was fired
+	sourceFunction string                 // name of the function from where the error was fired
+	lineNumber     int                    // line number where the error was fired
+}
+
+// New creates a new error composed of an error message and the stack trace
+func New(msg string) StackError {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	return StackError{
+		message:        msg,
+		sourceFilename: frame.File,
+		sourceFunction: frame.Function,
+		lineNumber:     frame.Line,
+	}
+}
+
+// Newf is like New() but it uses the Printf formatting
+func Newf(message string, args ...interface{}) StackError {
+	return New(fmt.Sprintf(message, args...))
+}
+
+func NewWithDetails(message string, details map[string]interface{}) StackError {
+	err := New(message)
+	err.details = details
+	return err
+}
+
+func (err StackError) Error() string {
+	return fmt.Sprintf("[%s:%d %s] : %s", err.sourceFilename, err.lineNumber, err.sourceFunction, err.message)
+}
