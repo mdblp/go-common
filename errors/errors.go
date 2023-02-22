e1a8f90f25277cc0a4dae1ee5780ee886d3b6774
ydris rebibane <ydris@diabeloop.fr>
2023-02-22 09:15:22 +0100

    Remove old tidepool errors (was a duplicate from the standard lib) and change the custom error signatures
    It is better to avoid repeating the error name in the New function.
    Wrap errors can now be used like this:
    ```
    import stackError "github.com/mdblp/go-common/errors"
    ...
    ...
    err := stackError.New("Oups an error")
    err := stackError.Newf("Oups this command '%s' failed", "do something")
    // wrap an existing error
    err := stackError.Wrap(srcErr)
    ```

--- errors/errors.go
+++ errors/errors.go
@@ -1,38 +1,13 @@
+// Package errors: custom errors which carry the stack trace in the error message
 package errors
 
 import (
-	"errors"
 	"fmt"
 	"runtime"
 )
 
-type WrappedError struct {
-	error
-	m string
-}
-
-func (err *WrappedError) Error() string {
-	return err.m + ": " + err.error.Error()
-}
-
-func New(message string) error {
-	return errors.New(message)
-}
-
-func Newf(message string, args ...interface{}) error {
-	return errors.New(fmt.Sprintf(message, args...))
-}
-
-func Wrap(e error, m string) error {
-	return &WrappedError{error: e, m: m}
-}
-
-func Wrapf(e error, m string, args ...interface{}) error {
-	return Wrap(e, fmt.Sprintf(m, args...))
-}
-
-// Create a new error which contains the stack trace
-func NewDetailedError(msg string) error {
+// New creates a new error composed of an error message and the stack trace
+func New(msg string) error {
 	pc := make([]uintptr, 15)
 	n := runtime.Callers(2, pc)
 	frames := runtime.CallersFrames(pc[:n])
@@ -40,7 +15,13 @@ func NewDetailedError(msg string) error {
 	return fmt.Errorf("[%s:%d %s] : %s", frame.File, frame.Line, frame.Function, msg)
 }
 
-func WrapDetailedError(errorToWrap error) error {
+// Newf is like New() but it uses the Printf formatting
+func Newf(message string, args ...interface{}) error {
+	return New(fmt.Sprintf(message, args...))
+}
+
+// Wrap returns an error based on an existing error and add stack trace details
+func Wrap(errorToWrap error) error {
 	pc := make([]uintptr, 15)
 	n := runtime.Callers(2, pc)
 	frames := runtime.CallersFrames(pc[:n])
