5bc2f145dc2c88b3ee6b60fe6fd72e34a654c163
Ydris Rebibane <ydris@diabeloop.fr>
2023-02-24 10:25:56 +0100

    New signature for wrapped errors (#40)
    * Deprecate old tidepool errors (was a duplicate from the standard lib) and create a new custom error
    
    It is better to avoid repeating the error name in the New function.
    Wrap errors can now be used like this:
    ```
    import stackError "github.com/mdblp/go-common/errors"
    ...
    ...
    err := stackError.New("Oups an error")
    err := stackError.Newf("Oups this command '%s' failed", "do something")
    // wrap an existing error
    err := stackError.Wrap(srcErr)
    ```
    
    * changelog
    
    
    Co-authored-by: Yann Blanc <yann.blanc@diabeloop.fr>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 # Go-Common
 
 Go-Common is the library of common functions for Tidepool's Go-based applications
+
+## 1.8.0 - 2023-02-24
+### Engineering
+- Deprecate go-common errors package
+- Add a new error type "StackError" which tracks details about the error source.
+
 ## 1.7.1 - 2023-02-21
 ### Engineering
 - New error wrapper utility
--- errors/errors.go
+++ errors/errors.go
@@ -15,18 +15,22 @@ func (err *WrappedError) Error() string {
 	return err.m + ": " + err.error.Error()
 }
 
+// Deprecated: all backloops services should use StackError instead
 func New(message string) error {
 	return errors.New(message)
 }
 
+// Deprecated: all backloops services should use StackError instead
 func Newf(message string, args ...interface{}) error {
 	return errors.New(fmt.Sprintf(message, args...))
 }
 
+// Deprecated: all backloops services should use StackError instead
 func Wrap(e error, m string) error {
 	return &WrappedError{error: e, m: m}
 }
 
+// Deprecated: all backloops services should use StackError instead
 func Wrapf(e error, m string, args ...interface{}) error {
 	return Wrap(e, fmt.Sprintf(m, args...))
 }
