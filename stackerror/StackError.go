83e7cbd922ed4d84c90185e937918be9fa078506
Yann Blanc <yann.blanc@diabeloop.fr>
2023-03-07 17:18:16 +0100

    test

--- stackerror/StackError.go
+++ stackerror/StackError.go
@@ -13,7 +13,7 @@ type ClientError interface {
 
 func NewLineError(message string) error {
 	pc := make([]uintptr, 15)
-	n := runtime.Callers(0, pc)
+	n := runtime.Callers(2, pc)
 	frames := runtime.CallersFrames(pc[:n])
 	frame, _ := frames.Next()
 	return fmt.Errorf("[%s:%d %s] %s", frame.File, frame.Line, frame.Function, message)
@@ -21,7 +21,7 @@ func NewLineError(message string) error {
 
 func WrapLineError(err error) error {
 	pc := make([]uintptr, 15)
-	n := runtime.Callers(0, pc)
+	n := runtime.Callers(2, pc)
 	frames := runtime.CallersFrames(pc[:n])
 	frame, _ := frames.Next()
 	return fmt.Errorf("[%s:%d %s] %w", frame.File, frame.Line, frame.Function, err)
