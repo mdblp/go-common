c565afd84ad9f9d23d15694385506da6389a3427
Yann Blanc <yann.blanc@diabeloop.fr>
2023-03-07 18:04:19 +0100

    test

--- stackerror/StackError.go
+++ stackerror/StackError.go
@@ -6,11 +6,6 @@ import (
 	"runtime"
 )
 
-type ClientError interface {
-	Type() string
-	Message() string
-}
-
 func NewLineError(message string) error {
 	pc := make([]uintptr, 15)
 	n := runtime.Callers(4, pc)
